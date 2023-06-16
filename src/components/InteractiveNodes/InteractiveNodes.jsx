f1fa48f1c807ca22b00c1553e4a8dd0a5a2e70f1
Udayasankar <udayasankar.j@emsibg.com>
2023-06-16 13:02:31 +0530

    Updated the Interactive Nodes for the size of the circle/node to fit the screen

--- src/components/InteractiveNodes/InteractiveNodes.jsx
+++ src/components/InteractiveNodes/InteractiveNodes.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./style.module.css";
 import CircleBox from "./CircleBox/CircleBox";
 
 export default function InteractiveNodes({ staff, management, executive }) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Update the windowWidth state when the window is resized
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      // Clean up the event listener on component unmount
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const getColumnX = (colNumber) => {
+    // Calculate the x position based on the screen width and column number
+    const columnWidth = windowWidth / 3;
+    return (colNumber - 1) * columnWidth + columnWidth/3;
+  };
+
   return (
     <div className={s.container}>
-      <svg viewBox="0 0 800 650" className={s.viewBox}>
+      <svg className={s.viewBox}>
         {staff &&
           staff.map((itm, index) => {
             const y = 100 + index * 100;
+            const x = getColumnX(1);
             return (
               <g key={index}>
                 <text
                   key={"ST" + itm.id}
-                  x="150"
+                  x={x}
                   y="20"
                   textAnchor="middle"
                   fill="white"
@@ -24,22 +47,23 @@ export default function InteractiveNodes({ staff, management, executive }) {
                 </text>
                 <CircleBox
                   key={"SC" + itm.id}
-                  x={150}
+                  x={x}
                   y={y}
                   item={itm}
                   colNumber={1}
-                ></CircleBox>
+                />
               </g>
             );
           })}
         {management &&
           management.map((itm, index) => {
             const y = 100 + index * 100;
+            const x = getColumnX(2);
             return (
               <g key={index}>
                 <text
                   key={"MT" + itm.id}
-                  x="400"
+                  x={x}
                   y="20"
                   textAnchor="middle"
                   fill="white"
@@ -50,22 +74,23 @@ export default function InteractiveNodes({ staff, management, executive }) {
                 </text>
                 <CircleBox
                   key={"MC" + itm.id}
-                  x={400}
+                  x={x}
                   y={y}
                   item={itm}
                   colNumber={2}
-                ></CircleBox>
+                />
               </g>
             );
           })}
         {executive &&
           executive.map((itm, index) => {
             const y = 70 + index * 100;
+            const x = getColumnX(3);
             return (
               <g key={index}>
                 <text
                   key={"ET" + itm.id}
-                  x="650"
+                  x={x}
                   y="20"
                   textAnchor="middle"
                   fill="white"
@@ -76,11 +101,11 @@ export default function InteractiveNodes({ staff, management, executive }) {
                 </text>
                 <CircleBox
                   key={"EC" + itm.id}
-                  x={650}
+                  x={x}
                   y={y}
                   item={itm}
                   colNumber={3}
-                ></CircleBox>
+                />
               </g>
             );
           })}
